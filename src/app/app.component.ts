4422ff0a67e5ebe3e7eb5f192831a65bb72b26df
Nimisha Wilson <nimishawilson123@gmail.com>
2024-07-14 06:05:26 +0530

    feat: use chip component to show skills

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProfileComponent } from './profile/profile.component';
+import { SkillsComponent } from './skills/skills.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ProfileComponent],
+  imports: [RouterOutlet, ProfileComponent, SkillsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,12 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ChipComponent } from '../components/chip/chip.component';
 
 @Component({
   selector: 'app-skills',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ChipComponent],
   templateUrl: './skills.component.html',
-  styleUrl: './skills.component.scss'
+  styleUrl: './skills.component.scss',
 })
 export class SkillsComponent {
-
+  skills = [
+    'Angular',
+    'NestJS',
+    'Html',
+    'Css',
+    'Javascript',
+    'Typescript',
+    'Tailwind',
+    'Bootstrap',
+    'Angular material',
+    'PrimeNG',
+    'RxJs',
+    'NgRx',
+    'git',
+  ];
 }
